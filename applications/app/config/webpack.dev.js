ba409615bc7f0e0335e6eac94246a90275025a6b
Krzysztof Len <len.krzysztof@gmail.com>
2022-01-17 21:09:10 +0100

    chore: init files

---
+++ applications/app/config/webpack.dev.js
@@ -0,0 +1,20 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const devConfig = {
+    mode: 'development',
+    output: {
+        publicPath: 'http://localhost:8080/',
+    },
+    devServer: {
+        port: 8080,
+        historyApiFallback: {
+            index: 'index.html',
+        },
+    },
+};
+
+module.exports = merge(commonConfig, devConfig);
---
+++ applications/app/src/bootstrap.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Welcome in Foodie App</h1>,
+    document.querySelector('#root'),
+);
