19f1bf32dd8448869c7d5b7e44fb155dadaa479a
Krzysztof Len <len.krzysztof@gmail.com>
2022-02-06 17:22:11 +0100

    chore: fix build script

--- applications/app/config/webpack.prod.js
+++ applications/app/config/webpack.prod.js
@@ -1,7 +1,8 @@
 const { merge } = require('webpack-merge');
 const { ModuleFederationPlugin } = require('webpack').container;
 const commonConfig = require('./webpack.common');
-const packageJson = require('../package.json');
+
+const deps = require('../package.json').dependencies;
 
 const domain = process.env.PRODUCTION_DOMAIN;
 
@@ -11,6 +12,29 @@ const prodConfig = {
         filename: '[name].[contenthash].js',
         publicPath: '/container/latest/', //TODO change it during implement AWS
     },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'app',
+            remotes: {
+                navigation: 'navigation@http://localhost:8081/remoteEntry.js',
+                list: 'list@http://localhost:8082/remoteEntry.js',
+                cookbook: 'cookbook@http://localhost:8083/remoteEntry.js',
+                cart: 'cart@http://localhost:8084/remoteEntry.js',
+                footer: 'footer@http://localhost:8085/remoteEntry.js',
+            },
+            shared: {
+                ...deps,
+                react: {
+                    singleton: true,
+                    requiredVersion: deps.react,
+                },
+                'react-dom': {
+                    singleton: true,
+                    requiredVersion: deps['react-dom'],
+                },
+            },
+        }),
+    ],
 };
 
 module.exports = merge(commonConfig, prodConfig);
--- applications/footer/config/webpack.prod.js
+++ applications/footer/config/webpack.prod.js
@@ -1,7 +1,8 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
-const packageJson = require('../package.json');
+
+const deps = require('../package.json').dependencies;
 
 const domain = process.env.PRODUCTION_DOMAIN;
 
@@ -11,6 +12,30 @@ const prodConfig = {
         filename: '[name].[contenthash].js',
         publicPath: '/container/latest/',
     },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'footer',
+            filename: 'remoteEntry.js',
+            remotes: {
+                navigation: 'navigation@http://localhost:8081/remoteEntry.js', //TODO change later for production remote
+            },
+            shared: {
+                ...deps,
+                react: {
+                    singleton: true,
+                    requiredVersion: deps.react,
+                },
+                'react-dom': {
+                    singleton: true,
+                    requiredVersion: deps['react-dom'],
+                },
+                primereact: {
+                    singleton: true,
+                    requiredVersion: deps.primereact,
+                },
+            },
+        }),
+    ],
 };
 
 module.exports = merge(commonConfig, prodConfig);
